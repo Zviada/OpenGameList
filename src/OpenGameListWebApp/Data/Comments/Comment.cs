8d560fdfc104beeb0403ae6dfe51550731e06e19
Eugeny <Eugeny@EUGENY->
2017-12-12 18:06:41 +0300

    Add comment EF entity

---
+++ src/OpenGameListWebApp/Data/Comments/Comment.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OpenGameListWebApp.Data.Comments
+{
+    public class Comment
+    {
+        public Comment()
+        {
+        }
+
+        #region Properties
+
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public int ItemId { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public int Type { get; set; }
+
+        [Required]
+        public int Flags { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public int? ParentId { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        [Required]
+        public DateTime LastModifiedDate { get; set; }
+
+        #endregion
+    }
+}
