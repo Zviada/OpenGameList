b9ce111076924a2867c8237ba89a9dfff668090d
Eugeny <Eugeny@EUGENY->
2017-12-13 21:46:38 +0300

    Added DB seeder class for filling database with some sample data

--- src/OpenGameListWebApp/Startup.cs
+++ src/OpenGameListWebApp/Startup.cs
@@ -38,32 +38,44 @@ namespace OpenGameListWebApp
 
             // Add ApplicationDbContext
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
+
+            services.AddSingleton<DbSeeder>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, DbSeeder dbSeeder)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
-            
+
             // Configure a rewrite rule to auto-lookup for standard default files such as index.html.
             app.UseDefaultFiles();
 
             // Configure a rewrite rule to auto-lookup for standard default files such as index.html.app.UseDefaultFiles();
             // Serve static files (html, css, js, images & more). See also the following URL: // https://docs.asp.net/en/latest/fundamentals/static-files.html for further reference.
             app.UseStaticFiles(new StaticFileOptions
-            {
-                OnPrepareResponse = context =>
-                {
-                    //Disable caching for all static files
-                    context.Context.Response.Headers["Cache-Control"] = Configuration["StaticFiles:Headers:Cache-Control"];
-                    context.Context.Response.Headers["Pragma"] = Configuration["StaticFiles:Headers:Pragma"];
-                    context.Context.Response.Headers["Expires"] = Configuration["StaticFiles:Headers:Expires"];
-                }
-            });
+                               {
+                                   OnPrepareResponse = context =>
+                                                       {
+                                                           //Disable caching for all static files
+                                                           context.Context.Response.Headers["Cache-Control"] =
+                                                               Configuration["StaticFiles:Headers:Cache-Control"];
+                                                           context.Context.Response.Headers["Pragma"] = Configuration["StaticFiles:Headers:Pragma"];
+                                                           context.Context.Response.Headers["Expires"] = Configuration["StaticFiles:Headers:Expires"];
+                                                       }
+                               });
 
             // Add MVC to the pipeline
             app.UseMvc();
+
+            try
+            {
+                dbSeeder.SeedAsync().Wait();
+            }
+            catch (AggregateException e)
+            {
+                throw new Exception(e.ToString());
+            }
         }
     }
 }
