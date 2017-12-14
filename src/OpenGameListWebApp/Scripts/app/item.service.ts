64b842a05ffc50419a0b273bc903264f2e2f2f42
Eugeny <Eugeny@EUGENY->
2017-12-14 15:31:07 +0300

    added new RESTful methods to client ItemService

--- src/OpenGameListWebApp/Scripts/app/item.service.ts
+++ src/OpenGameListWebApp/Scripts/app/item.service.ts
@@ -1,16 +1,16 @@
 ﻿import { Injectable } from "@angular/core";
-import { Http, Response } from "@angular/http";
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from "rxjs/Observable";
 import { Item } from "./item";
 
 @Injectable()
 export class ItemService {
 
-    constructor(private http: Http) { }
-
     private baseUrl = 'api/items/'; //webapi URL
 
-	//calls the [GET] /api/items/GetLatest/{n}
+    constructor(private http: Http) { }
+
+    //calls the [GET] /api/items/GetLatest/{n}
 	getLatest(num?: number) {
         return this.getItems('GetLatest', num);
     }
@@ -35,7 +35,28 @@ export class ItemService {
 	    return this.http.get(url)
 	        .map(res => <Item>res.json())
 	        .catch(this.handleError);
-	}
+    }
+
+    //calls the [POST] /api/items/ Web API method to add a new item
+    add(item: Item) {
+        return this.http.post(this.baseUrl, JSON.stringify(item), this.getRequestOptions())
+            .map(response => response.json())
+            .catch(this.handleError);
+    }
+
+    //calls the [PUT] /api/items/{id} Web API method to update an existing item
+    update(item: Item) {
+        const url = this.baseUrl + item.Id;
+        return this.http.put(url, JSON.stringify(item), this.getRequestOptions())
+            .map(response => response.json())
+            .catch(this.handleError);
+    }
+
+    //calls the [DELETE] /api/items/{id} Web API method to delete the item with given id
+    delete(id: number) {
+        const url = this.baseUrl + id;
+        return this.http.delete(url).catch(this.handleError);
+    }
 
 	//makes a GET call to /api/items/{actionName}/{n}
     private getItems(actionName: string, num?: number) {
@@ -43,6 +64,14 @@ export class ItemService {
         return this.http.get(url)
             .map(response => <Item>response.json())
             .catch(this.handleError);
+    }
+
+    private getRequestOptions() {
+        return new RequestOptions({
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        });
     }
 
     private handleError(error: Response) {
